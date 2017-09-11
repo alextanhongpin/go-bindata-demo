b011dba4f3cb93eaa0e7c5a0525766a1d80ba0f5
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-09-11 10:25:35 +0800

    Initial Commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/alextanhongpin/go-binddata-demo/asset"
+)
+
+type People struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+//go:generate go-bindata -o asset/bindata.go data/
+func main() {
+	log.Println("hello world")
+
+	data, err := asset.Asset("data/data.json")
+	if err != nil {
+		log.Println(err)
+	}
+	var people []People
+	err = json.Unmarshal(data, &people)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(people)
+}
